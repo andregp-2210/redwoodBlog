526730de619f95e308d1da859d57418446beb85f
André <andr3_andriu@hotmail.com>
2022-07-29 17:39:48 -0500

    feat: setup project

---
+++ web/src/pages/HomePage/HomePage.js
@@ -0,0 +1,12 @@
+import { MetaTags } from '@redwoodjs/web'
+
+const HomePage = () => {
+  return (
+    <>
+      <MetaTags title="Home" description="Home page" />
+      <p>Welcome to the blog</p>
+    </>
+  )
+}
+
+export default HomePage
